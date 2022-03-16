f6e106aae49af286622b018a394315c99c0773a8
Ant <it@me.org>
2022-03-15 21:57:50 -0500

    Cleanup unused code

--- src/main/scala/BlogService.scala
+++ src/main/scala/BlogService.scala
@@ -6,7 +6,7 @@ import cats.*
 import doobie.{Meta, Read}
 import io.circe.generic.semiauto.deriveCodec
 import org.http4s.{EntityDecoder, EntityEncoder}
-import repo.BlogsRepo.{Blogs}
+import repo.BlogsRepo.Blogs
 //import cats.syntax.option._
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
@@ -18,25 +18,6 @@ import io.circe.*
 
 
 class BlogService(repository: Blogs[IO]) extends Http4sDsl[IO] {
-  //    import io.circe._, io.circe.generic.semiauto._
-
-//  val richList = List(
-//    RichBlog(BlogId(1), Title("hello"), Content("hey")),
-//    RichBlog(BlogId(2), Title("heyyy"), Content("hello"))
-//  )
-
-
-  //  case class Name(value: String)
-  //  case class Age(value: Int)
-
-
-  //  implicit val personEncoder: Encoder[Person] = person => Json.obj(
-  //    "name" -> person.name.asJson,
-  //    "age" -> person.age.asJson,
-  //    "weightitb" -> person.weight.asJson
-  //  )
-  //
-//    implicit val personDecoder: Decoder[Person] = Decoder.forProduct4("id", "name", "age", "weight")()
   implicit val personDecoder: EntityDecoder[IO, Person] = jsonOf[IO, Person]
 
   val peeps = List(
@@ -49,39 +30,7 @@ class BlogService(repository: Blogs[IO]) extends Http4sDsl[IO] {
     KewlBlog(KewlId(2), KewlTitle("itb pt 2"), KewlContent("sfdfs"))
   )
 
-
-
-
-//  implicit val blogIdCode: Codec[BlogId] = deriveCodec[BlogId]
-//  implicit val blogCodec: Codec[RichBlog] = deriveCodec[RichBlog]
-
-
-  //
-  //  implicit val fooDecoder: Decoder[Name] = deriveDecoder[Name]
-  //  implicit val fooEncoder: Encoder[Name] = deriveEncoder[Name]
-  //
-  //
-  //  implicit val Decoder: Decoder[Person] = deriveDecoder[Person]
-  //  implicit val Encoder: Encoder[Person] = deriveEncoder[Person]
-
-  //    implicit val fooEncoder: Encoder[RichBlog] = deriveEncoder[RichBlog]
-  //    implicit val fooDecoder: Decoder[RichBlog] = deriveDecoder[RichBlog]
-
-  //      implicit val fooEncoder: Encoder[RichBlog] = jsonOf
-  //      implicit val fooDecoder: Decoder[RichBlog] = jsonOf
-
-  //  implicit val encoder: Encoder[TestSprout] = Encoder[TestSprout].contramap(x => x)
-  //  implicit val decoder: Decoder[TestSprout] = Decoder[String].map(TestSprout(_))
-  //  implicit val entityEncoder: EntityEncoder[IO, TestSprout] = jsonEncoderOf[IO, TestSprout]
-  //  implicit val entityDecoder: EntityDecoder[IO, TestSprout] = jsonOf[IO, TestSprout]
-
-  //  implicit val natMeta: Meta[BlogId] = Meta[Int].imap(fromInt)(toInt)
-
-
-  //  implicit val richBlogRead: Read[RichBlog] =
-  //    Read[(Int, String, String)].map { case (id, title, content) => RichBlog(BlogId(id), Title(title), Content(content)) }
-  //
-  val routes = HttpRoutes.of[IO] {
+  val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
     case GET -> Root / "peeps" => Ok(peeps)
     case req @ POST -> Root / "peeps" =>
       for {
@@ -112,28 +61,3 @@ class BlogService(repository: Blogs[IO]) extends Http4sDsl[IO] {
       } yield res
   }
 }
-//
-//    case GET -> Root / "rich" / IntVar(id) =>
-//      for {
-//        b <- repository.findRichIdById(id)
-//        c <- b.fold(NotFound())(x => Ok(x.value))
-//      } yield c
-//
-//    case GET -> Root / "rich" => Ok(peeps)
-//      for {
-//        x <- Ok(peeps.map(personDto))
-////        y <- Ok(x)
-//      } yield x
-//
-//    case GET -> Root / "richBlogs" =>
-//      for {
-//        w <- repository.findAllRichBlogs()
-//        x <- Ok(w.map(blogToDto))
-//        //        y <- Ok(x)
-//      } yield x
-
-
-
-//}
-
-
