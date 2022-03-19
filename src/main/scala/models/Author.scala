9141807ab1320bbfccfa3083ad13bff901abfac9
Ant <it@me.org>
2022-03-19 13:22:50 -0500

    Get email status to serialize right

--- src/main/scala/models/Author.scala
+++ src/main/scala/models/Author.scala
@@ -1,18 +1,26 @@
 package models
 
 import doobie.Read
-import io.circe.{Codec, Decoder}
+import io.circe.{Codec, Decoder, Encoder}
 import io.circe.generic.semiauto.{deriveCodec, deriveDecoder}
+
+import io.circe.syntax.*
+import io.circe.generic.auto.*
 
 object Author:
   implicit val authorCodec: Codec[Author] = deriveCodec[Author]
-//  implicit val emailCodec: Decoder[Email] = deriveDecoder[Email]
+  implicit val emailCodec: Codec[Email] = deriveCodec[Email]
+//  implicit val statusCodec: Codec[VerificationStatus] = deriveCodec[VerificationStatus]
+  implicit val statusDecode: Encoder[VerificationStatus] = Encoder.instance {
+    case verified @ Verified() => verified.asJson
+    case unverified @ Unverified(_) => unverified.asJson
+  }
   implicit val authorRead: Read[Author] =
-    Read[(Int, String, String, String)].map { case (id, name, email, verified) =>
-//      val status = VerificationStatus.fromString(emailStatus)
-      // , Email(EmailAddress(emailAddress), EmailVerification(emailStatus))
-//      val email = Email(EmailAddress(emailAdd), EmailVerification(verified))
-      Author(AuthorId(id), Name(name), EmailAddress(email), EmailVerification(verified))
+    Read[(Int, String, String, String)].map { case (id, name, emailAdd, verified) =>
+      val status = EmailVerification.make(verified)
+      val hmm = EmailVerification.cake("")
+      val email = Email(EmailAddress(emailAdd), status)
+      Author(AuthorId(id), Name(name), email)
     }
 
   opaque type Name = String
@@ -47,16 +55,28 @@ object Author:
   opaque type EmailVerification = String
   object EmailVerification:
     def apply(value: String): EmailVerification = value
+    def make(input: String): EmailVerification = input match {
+      case "Verified" => new EmailVerification("Verified")
+      case _ => new EmailVerification("Unverified")
+    }
+    val cake: String => EmailVerification = {
+      case "verified" => new EmailVerification("Verified")
+      case _ => new EmailVerification("Unverified")
+    }
 
-  enum VerificationStatus:
-    case Verified
-    case Unverified
+//  enum VerificationStatus:
+//    case Verified
+//    case Unverified
+
+  sealed trait VerificationStatus
+  case class Unverified(uv: String) extends VerificationStatus
+  case class Verified() extends VerificationStatus
 
   object VerificationStatus:
     def fromString(string: String): VerificationStatus = string match
-      case "Unverified" => VerificationStatus.Unverified
-      case "Verified" => VerificationStatus.Verified
+      case "Unverified" => Unverified("Unverified")
+      case "Verified" => Verified()
 
-  case class Email(address: EmailAddress, verified: EmailVerification)
+  case class Email(address: EmailAddress, emailStatus: EmailVerification)
 
-  case class Author(id: AuthorId, name: Name, emailAddress: EmailAddress, emailVerification: EmailVerification)
+  case class Author(id: AuthorId, name: Name, email: Email)
