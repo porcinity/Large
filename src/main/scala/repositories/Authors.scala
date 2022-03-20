eef213650cfee9bcdaa4b99ff5cbf64575162ce9
Ant <it@me.org>
2022-03-20 17:02:21 -0500

    Update repo to account for new type of id

--- src/main/scala/repositories/Authors.scala
+++ src/main/scala/repositories/Authors.scala
@@ -10,38 +10,39 @@ import cats.syntax.functor.*
 
 trait Authors[F[_]]:
   def findAllAuthors: F[List[Author]]
-  def findAuthorById(id: Int): F[Option[Author]]
+  def findAuthorById(id: String): F[Option[Author]]
   def create(author: Author): F[Author]
   def update(author: Author): F[Author]
-  def delete(id: Int): F[Either[String, Int]]
+  def delete(id: String): F[Either[String, Int]]
 //  def verify(author: Author): F[Unit]
 
 object Authors:
   def make[F[_]: Concurrent](postgres: Resource[F, Transactor[F]]): Authors[F] =
     new Authors[F] {
       override def findAllAuthors: F[List[Author]] = postgres.use { xa =>
-        sql"select author_id, author_name, author_email, author_email_status from authors"
+        sql"select author_id, author_name, author_email, author_email_status, author_join_date from authors"
           .query[Author].to[List].transact(xa)
       }
 
-      override def findAuthorById(id: Int): F[Option[Author]] = postgres.use { xa =>
-        sql"select author_id, author_name, author_email, author_email_status from authors where author_id = $id"
+      override def findAuthorById(id: String): F[Option[Author]] = postgres.use { xa =>
+        sql"select author_id, author_name, author_email, author_email_status, author_join_date from authors where author_id = $id"
           .query[Author].option.transact(xa)
       }
 
       override def create(author: Author): F[Author] = postgres.use { xa =>
         val id = author.id.value
         val name = author.name.value
-        val email = author.email.address.value
+        val email = author.email.address.v
         val emailStatus = EmailStatus.makeString(author.email.status)
-        sql"insert into authors (author_id, author_name, author_email, author_email_status) values ($id, $name, $email, $emailStatus)"
-          .update.withUniqueGeneratedKeys("author_id", "author_name", "author_email", "author_email_status").transact(xa)
+        val joinDate = author.joinDate.value
+        sql"insert into authors (author_id, author_name, author_email, author_email_status, author_join_date) values ($id, $name, $email, $emailStatus, $joinDate)"
+          .update.withUniqueGeneratedKeys("author_id", "author_name", "author_email", "author_email_status", "author_join_date").transact(xa)
       }
 
       override def update(author: Author): F[Author] = postgres.use { xa =>
         val id = author.id.value
         val name = author.name.value
-        val email = author.email.address.value
+        val email = author.email.address.v
         val status = EmailStatus.makeString(author.email.status)
         sql"update authors set author_name = $name, author_email = $email, author_email_status = $status where author_id = $id"
           .update.withUniqueGeneratedKeys("author_id", "author_name", "author_email", "author_email_status").transact(xa)
@@ -53,7 +54,7 @@ object Authors:
 //        sql"update authors set author_email_status = $status where author_id = $id".run
 //      }
 //
-      override def delete(id: Int): F[Either[String, Int]] = postgres.use { xa =>
+      override def delete(id: String): F[Either[String, Int]] = postgres.use { xa =>
         sql"delete from authors where author_id = $id"
           .update.run.transact(xa).map { x =>
             if (x == 0) Left("Blog not found.")
