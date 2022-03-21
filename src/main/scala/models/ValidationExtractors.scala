0be23882350f8a65b5ca893a8619b9853be21a5b
Ant <it@me.org>
2022-03-21 17:51:15 -0500

    Add Validation Extractors

---
+++ src/main/scala/models/ValidationExtractors.scala
@@ -0,0 +1,20 @@
+package models
+
+object ValidationExtractors:
+  object FewerThan5:
+    def unapply(x: String): Boolean = x.length < 5 && x.nonEmpty
+
+  object Over100:
+    def unapply(x: String): Boolean = x.length > 100
+    
+  object Over150:
+    def unapply(x: String): Boolean = x.length > 150
+    
+  object Over15k:
+    def unapply(x: String): Boolean = x.length > 15000
+
+  object NumbersOrChars:
+    def unapply(x: String): Boolean = !x.matches("^[a-zA-Z]+$")
+
+  object EmptyName:
+    def unapply(x: String): Boolean = x.isEmpty
