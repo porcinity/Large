7cdc3efd76ac616c2a8f35130c0a9c1658b34640
Ant <it@me.org>
2022-03-23 07:34:26 -0500

    Test out Monocle

--- src/main/scala/routes/BlogService.scala
+++ src/main/scala/routes/BlogService.scala
@@ -15,6 +15,14 @@ import repositories.Blogs
 // These are necessary to use for-comprehensions on F
 import cats.syntax.flatMap.*
 import cats.syntax.functor.*
+//import cats.syntax.*
+import cats.implicits.*
+
+
+//import monocle.syntax.*
+import monocle.Lens
+import monocle.macros.GenLens
+import monocle.macros.syntax.AppliedFocusSyntax
 
 // The type constraint of Concurrent is necessary to decode Json
 class BlogService[F[_]: Concurrent](repository: Blogs[F]) extends Http4sDsl[F] {
@@ -32,11 +40,11 @@ class BlogService[F[_]: Concurrent](repository: Blogs[F]) extends Http4sDsl[F] {
       } yield res
 
     case req @ POST -> Root =>
-      for {
+      for
         dto <- req.decodeJson[BlogDto]
-        blog = BlogDto.toDomain(dto)
+        blog <- BlogDto.toDomain(dto).pure[F]
         res <- blog.fold(UnprocessableEntity(_), b => Created(repository.insertBlog(b)))
-      } yield res
+      yield res
 
     case req @ PUT -> Root / BlogIdVar(id) =>
       for {
@@ -47,8 +55,11 @@ class BlogService[F[_]: Concurrent](repository: Blogs[F]) extends Http4sDsl[F] {
           case (None, _) => NotFound()
           case (_, Invalid(e)) => UnprocessableEntity(e)
           case (Some(b), Valid(u)) =>
+            val blogTitle = Lens[Blog, BlogTitle](_.title)(t => b => b.copy(title = t))
+            val blogContent = Lens[Blog, BlogContent](_.content)( c => b => b.copy(content = c))
             val newBlog = b.copy(title = u.title, content = u.content)
-            Created(repository.update(newBlog))
+            val lensyBoi = blogTitle.replace(u.title)(b)
+            Created(repository.update(lensyBoi))
       } yield res
 
     case DELETE -> Root / BlogIdVar(id) =>
