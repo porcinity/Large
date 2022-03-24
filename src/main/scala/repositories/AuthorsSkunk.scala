9aee89e6048b2b4798a174803024ef4184f896d3
Ant <it@me.org>
2022-03-24 10:17:40 -0500

    Skunk is minimally viable

--- src/main/scala/repositories/AuthorsSkunk.scala
+++ src/main/scala/repositories/AuthorsSkunk.scala
@@ -1,7 +1,7 @@
 package repositories
 
 
-import cats.effect.{Resource, Concurrent}
+import cats.effect.{Resource, Concurrent, MonadCancelThrow}
 import models.Author.*
 import skunk.*
 import skunk.implicits.*
@@ -9,13 +9,20 @@ import skunk.codec.text.*
 import skunk.codec.temporal.*
 
 trait AuthorsSkunk[F[_]]:
-  def findAllAuthorsSkunk: F[List[Author]]
+  def findAllAuthors: F[List[Author]]
+  def findAuthorById(id: AuthorId): F[Option[Author]]
 
 object AuthorsSkunk:
   import AuthorSql.*
   def make[F[_]: Concurrent](pg: Resource[F, Session[F]]): AuthorsSkunk[F] =
     new AuthorsSkunk[F] {
-      override def findAllAuthorsSkunk: F[List[Author]] = pg.use(_.execute(selectAll))
+      override def findAllAuthors: F[List[Author]] = pg.use(_.execute(selectAll))
+      
+      override def findAuthorById(id: AuthorId): F[Option[Author]] = pg.use { session =>
+        session.prepare(selectById).use { ps =>
+          ps.option(id)
+        }
+      }
     }
 
 private object AuthorSql:
@@ -33,5 +40,11 @@ private object AuthorSql:
         )
     }
 
+  val authorId: Codec[AuthorId] =
+    varchar.imap[AuthorId](AuthorId(_))(_.value)
+
   val selectAll: Query[Void, Author] =
     sql"select * from authors".query(decoder)
+
+  val selectById: Query[AuthorId, Author] =
+    sql"select * from authors where author_id = $authorId".query(decoder)
