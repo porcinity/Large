0c3ecc72a6d0a66f0a5d7a1c0df156a00be4db53
Ant <it@me.org>
2022-03-28 13:33:02 -0500

    Add tags repo

---
+++ src/main/scala/repositories/Tags.scala
@@ -0,0 +1,38 @@
+package repositories
+
+import cats.effect.{Concurrent, Resource}
+import models.Tag.{Tag, TagName}
+import repositories.Codecs.tagName
+import skunk.codec.text.varchar
+import skunk.{Codec, Command, Decoder, Query, Session, Void}
+import skunk.implicits.*
+
+trait Tags[F[_]]:
+  def findAllTags: F[List[TagName]]
+//  def createTag(name: String): F[Option[TagName]]
+//  def updateTag(name: String): F[Unit]
+//  def delete(tag: Tag): F[Unit]
+
+object Tags:
+  import TagsSql.*
+  def make[F[_]: Concurrent](postgres: Resource[F, Resource[F, Session[F]]]): Tags[F] =
+    new Tags[F] {
+      override def findAllTags: F[List[TagName]] = postgres.use(_.use(_.execute(selectAll)))
+
+//      override def createTag(name: String): F[Option[Tag]] = ???
+
+//      override def updateTag(name: String): F[Unit] = ???
+//
+//      override def delete(tag: Tag): F[Unit] = ???
+    }
+
+private object TagsSql:
+
+  val selectAll: Query[Void, TagName] =
+    sql"select * from tags".query(tagName)
+
+  val insert: Command[TagName] =
+    sql"insert into tags values ($tagName)".command
+
+//  val updateTag: Command[TagName] =
+//    sql"update tags set tag_name = $codec where tag_name = $varchar".command
