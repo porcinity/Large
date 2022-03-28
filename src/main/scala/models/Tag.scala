9473a2b283e960fbf9aa8d548bea442f66ab7be9
Ant <it@me.org>
2022-03-28 13:33:25 -0500

    Update tag model

--- src/main/scala/models/Tag.scala
+++ src/main/scala/models/Tag.scala
@@ -1,47 +1,53 @@
 package models
 
-import models.Blog.*
+import models.Note.*
 import io.circe.Codec
 import io.circe.generic.semiauto.deriveCodec
 import com.aventrix.jnanoid.jnanoid.*
 
 import scala.annotation.targetName
 
-object Tag:
+object Tag {
+
+  case class Tag(id: TagId, name: TagName, noteId: TaggedNote)
+
   implicit val tagCodec: Codec[Tag] = deriveCodec[Tag]
   opaque type TagId = String
+
   object TagId:
     def apply(value: String): TagId = value
 
   opaque type TagName = String
+
   object TagName:
     def apply(value: String): TagName = value
+  extension (x: TagName)
+    @targetName("value_TagName")
+    def value: String = x
 
-  opaque type TaggedBlog = String
-  object TaggedBlog:
-    def apply(value: String): TaggedBlog = value
+  opaque type TaggedNote = String
+
+  object TaggedNote:
+    def apply(value: String): TaggedNote = value
 
   extension (x: TagId)
     @targetName("value_TagId")
     def value: String = x
 
-  extension (x: TagName)
-    @targetName("value_TagName")
+  extension (x: TaggedNote)
+    @targetName("value_TaggedNote")
     def value: String = x
-
-  extension (x: TaggedBlog)
-    @targetName("value_blogId")
-    def value: String = x
-
-  case class Tag(id: TagId, name: TagName, blogId: TaggedBlog)
 
   case class TagDto(name: String)
+
   object TagDto:
     implicit val tagDtoCodec: Codec[TagDto] = deriveCodec[TagDto]
-    def toDomain(dto: TagDto, id: BlogId): Tag =
+
+    def toDomain(dto: TagDto, id: NoteId): Tag =
       val tagId = NanoIdUtils.randomNanoId()
       Tag(
         TagId(tagId),
         TagName(dto.name),
-        TaggedBlog(id.value)
+        TaggedNote(id.value)
       )
+}
