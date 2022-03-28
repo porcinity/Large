fabd12d742b62415cb0d4c2d5511d16a2183ade0
Ant <it@me.org>
2022-03-28 17:52:48 -0500

    Update tags routes with manual enveloped json object

--- src/main/scala/routes/TagsRoutes.scala
+++ src/main/scala/routes/TagsRoutes.scala
@@ -7,17 +7,27 @@ import models.Tag.TagDto.*
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import repositories.Tags
-import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.circe.*
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.*
 import org.http4s.Status.*
 import org.http4s.implicits.*
 import cats.syntax.all.*
-
+import io.circe.Json
+import io.circe.syntax.*
+import io.circe.parser.*
+import cats.syntax.flatMap.*
+import cats.syntax.functor.*
 class TagsRoutes[F[_]: Concurrent](repository: Tags[F]) extends Http4sDsl[F]:
 
   val routes: HttpRoutes[F] = HttpRoutes.of[F] {
-    case GET -> Root => Ok(repository.findAllTags)
+    case GET -> Root =>
+      for {
+        tags <- repository.findAllTags
+        json <- Json.obj(("data", tags.asJson)).pure[F]
+        res <- Ok(json)
+      } yield res
+//      Ok(repository.findAllTags)
     case req @ POST -> Root =>
       for {
         dto <- req.asJsonDecode[TagDto]
