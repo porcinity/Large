2c7202dac1eba56a58bfc99390303d360584f5a7
Ant <it@me.org>
2022-03-28 11:04:25 -0500

    Clean up Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,11 @@
-import cats.effect.{ExitCode, IO, IOApp, Resource, Sync}
-import doobie.hikari.HikariTransactor
-import doobie.util.ExecutionContexts
-import models.Blog.*
+import cats.effect.{ExitCode, IO, IOApp, Resource}
 import org.http4s.HttpRoutes
 import org.http4s.ember.server.*
 import org.http4s.implicits.*
 import org.http4s.syntax.all.*
 import org.http4s.server.Router
-import repositories.{BlogsSkunk, Users, UsersSkunk}
-import routes.{BlogService, UserService}
+import repositories.{Notes, UsersSkunk}
+import routes.{NotesRoutes, UserService}
 import com.comcast.ip4s.{ipv4, port}
 import skunk.*
 import skunk.codec.text.*
@@ -18,17 +15,6 @@ import org.http4s.server.middleware.Logger
 
 object Main extends IOApp:
   override def run(args: List[String]): IO[ExitCode] =
-
-    val postgres: Resource[IO, HikariTransactor[IO]] = for {
-      ce <- ExecutionContexts.fixedThreadPool[IO](32)
-      xa <- HikariTransactor.newHikariTransactor[IO](
-        "org.postgresql.Driver",
-        "jdbc:postgresql:blog",
-        "anthony",
-        "itb",
-        ce
-      )
-    } yield xa
 
     val session: Resource[IO, Resource[IO, Session[IO]]] =
       Session.pooled[IO](
@@ -40,20 +26,16 @@ object Main extends IOApp:
         max = 10
       )
 
-//    val blogsRepo: Blogs[IO] = Blogs.make(postgres)
+    val usersRepo: UsersSkunk[IO] = UsersSkunk.make(session)
 
-    val usersRepo: Users[IO] = Users.make(postgres)
+    val notesRepo: Notes[IO] = Notes.make(session)
 
-    val skunkRepo: UsersSkunk[IO] = UsersSkunk.make(session)
+    val notesService: NotesRoutes[IO] = new NotesRoutes(notesRepo)
 
-    val blogsSkunk: BlogsSkunk[IO] = BlogsSkunk.make(session)
-
-    val blogService: BlogService[IO] = new BlogService(blogsSkunk)
-
-    val userService: UserService[IO] = new UserService(skunkRepo)
+    val userService: UserService[IO] = new UserService(usersRepo)
 
     val httpApp = Router(
-      "/blogs" -> blogService.routes,
+      "/notes" -> notesService.routes,
       "/users" -> userService.routes
     ).orNotFound
 
