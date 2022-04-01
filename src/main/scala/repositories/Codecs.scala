cdf58a052fae9865e9c688ee9f81a14c31436a92
Ant <it@me.org>
2022-04-01 17:14:00 -0500

    Update Skunk codecs

--- src/main/scala/repositories/Codecs.scala
+++ src/main/scala/repositories/Codecs.scala
@@ -1,6 +1,7 @@
 package repositories
 
-import models.Note.{NoteAuthor, NoteId}
+import models.Note
+import models.Note.Author
 import models.Tag.TagName
 import models.User.UserId
 import skunk.Codec
@@ -10,11 +11,11 @@ object Codecs:
   val userId: Codec[UserId] =
     varchar.imap[UserId](UserId.unsafeFrom)(_.value)
 
-  val blogAuthorId: Codec[NoteAuthor] =
-    varchar.imap[NoteAuthor](NoteAuthor(_))(_.value)
+  val blogAuthorId: Codec[Author] =
+    varchar.imap[Author](Author.unsafeFrom)(_.value)
 
-  val noteId: Codec[NoteId] =
-    varchar.imap[NoteId](NoteId(_))(_.value)
+  val noteId: Codec[Note.Id] =
+    varchar.imap[Note.Id](Note.Id.unsafeFrom)(_.value)
 
   val tagName: Codec[TagName] =
     varchar.imap[TagName](TagName(_))(_.value)
